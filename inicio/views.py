d48b5be192ae53ee730ecb6b2b909875a343b31a
wminuet <walterminuet@gmail.com>
2024-02-20 05:30:54 -0300

    Actualizacion de la app

--- inicio/views.py
+++ inicio/views.py
@@ -1,14 +1,30 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from django.template import Template
+from django.template import Template, Context, loader
 
 # Create your views here.
 def inicio(request):
-    
-    archivo_abierto = open(r'C:\Users\walte\Documents\Entrega_3\templates\inicio.html', 'r')
-    
-    template = Template(archivo_abierto.read())
-    
-    archivo_abierto.close()
-    
-    return HttpResponse('')
+    # V1
+    #archivo_abierto = open(r'C:\Users\walte\Documents\Entrega_3\templates\inicio.html', 'r')
+    #template = Template(archivo_abierto.read())
+    #archivo_abierto.close()
+    #contexto = Context()
+    #template_renderizado = template.render(contexto)
+    #return HttpResponse(template_renderizado)
+
+    #v2 cargadores    
+    #template = loader.get_template('inicio.html')
+  
+    #dicc = {
+    #    'nombre': 'Carlos',
+    #    'apellido': 'Perez'
+    #}
+    #template_renderizado = template.render(dicc)
+    #return HttpResponse(template_renderizado)
+
+    #v3 render
+    dicc = {
+        'nombre': 'Carlos',
+        'apellido': 'Perez'
+    }
+    return render(request, 'inicio.html', dicc)
